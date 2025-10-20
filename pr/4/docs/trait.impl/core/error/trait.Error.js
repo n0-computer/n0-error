34755088e65c7769885ffc6a310485c7723f7ac5
Frando <Frando@users.noreply.github.com>
2025-10-20 10:18:37 +0000

    deploy: 5144fdde0a1c03fe32f262ab1562a484f2b30598

---
+++ pr/4/docs/trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["n0_error",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"n0_error/struct.AnyError.html\" title=\"struct n0_error::AnyError\">AnyError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[270]}
