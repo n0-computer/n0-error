2909e3f12de2940aa0176ce5f6bbcb064c7c1910
Frando <frando@unbiskant.org>
2025-10-30 23:57:18 +0100

    fixup

--- n0-error-macros/src/lib.rs
+++ n0-error-macros/src/lib.rs
@@ -33,7 +33,7 @@ use syn::{
 ///     #[error("io failed")]
 ///     Io { source: std::io::Error },
 ///     #[error("remote failed with {_0}")]
-///     RemoteErrorCode(u32)
+///     RemoteErrorCode(u32),
 /// }
 /// ```
 /// expands to
@@ -42,9 +42,9 @@ use syn::{
 /// #[error(from_sources)]
 /// enum MyError {
 ///     #[error("io failed")]
-///     Io { source: std::io::Error, meta: n0_error::Meta }
+///     Io { source: std::io::Error, meta: n0_error::Meta },
 ///     #[error("remote failed with {_0}")]
-///     RemoteErrorCode(u32, #[error(meta)] n0_error::Meta)
+///     RemoteErrorCode(u32, #[error(meta)] n0_error::Meta),
 /// }
 /// ```
 #[proc_macro_attribute]
